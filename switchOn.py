cd72c32aab8d7062d7f32c693d1b5222c96ab94c
marco-silvestri <m.silvestri03@gmail.com>
2020-10-09 17:06:24 +0200

    action fix

--- switchOn.py
+++ switchOn.py
@@ -9,20 +9,20 @@ state = 0
 if state == 0:
     relay.on()
     state = 1
-    sleep(10)
-    nasMount = subprocess.Popen(['mount /dev/sda2 /server_mount/nas', 'More output'],
+    #sleep(10)
+    #nasMount = subprocess.Popen(['mount /dev/sda2 /server_mount/nas', 'More output'],
                     stdout=subprocess.PIPE, 
                     stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    stdout, stderr
+    #stdout, stderr = process.communicate()
+    #stdout, stderr
     sys.exit(1)
 elif state == 1:
-    sleep(10)
-    nasMount = subprocess.Popen(['umount /dev/sda2 /server_mount/nas', 'More output'],
-                    stdout=subprocess.PIPE, 
-                    stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    stdout, stderr
+    #sleep(10)
+    #nasMount = subprocess.Popen(['umount /dev/sda2 /server_mount/nas', 'More output'],
+    #                stdout=subprocess.PIPE, 
+    #                stderr=subprocess.PIPE)
+    #stdout, stderr = process.communicate()
+    #stdout, stderr
     relay.off()
     state = 0
     sys.exit(0)
