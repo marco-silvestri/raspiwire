cc57d234e87dc3d5877c85aab270d94c99c14780
marco-silvestri <m.silvestri03@gmail.com>
2020-10-10 19:24:01 +0200

    dynamic pin loading w/ Livewire, added NodeJS Gpio.onoff fix#10

--- toggle.js
+++ toggle.js
@@ -10,11 +10,6 @@ let state = myArgs[1];
 toggleGpioState();
 
 function toggleGpioState(){
-    if (pinOut.readSync() === 0){ //check the pin state, if the state is 0 (or off)
-        state = 1;
-    } else {
-        state = 0;
-    }
     pinOut.writeSync(state); //set pin state
     console.log("State is: " + state);
 }
