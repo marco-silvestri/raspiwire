7cae7b6268b7ea9903827c37939893672843582a
marco-silvestri <m.silvestri03@gmail.com>
2020-10-10 19:49:00 +0200

    Toggle works. To do mount/umount cmd

--- toggle.js
+++ toggle.js
@@ -5,11 +5,11 @@ let pinNumber = myArgs[0];
 let Gpio = require('./node_modules/onoff/onoff').Gpio; //include onoff to interact with the GPIO
 
 let pinOut = new Gpio(pinNumber, 'out'); //use GPIO pin passed by blade, and specify that it is output
-let state = myArgs[1];
+let state = parseInt(myArgs[1]);
 
 toggleGpioState();
 
 function toggleGpioState(){
     pinOut.writeSync(state); //set pin state
-    console.log("State is: " + state);
+    console.log("State is: " + pinOut.readSync());
 }
