9363e9a6304f46f642698a1333cd6d7d0e8874d9
marco-silvestri <m.silvestri03@gmail.com>
2022-06-05 18:05:52 +0200

    fixed views

--- resources/views/livewire/nas.blade.php
+++ resources/views/livewire/nas.blade.php
@@ -42,29 +42,22 @@
         <form wire:submit.prevent="toggle">
             <input type="hidden" wire:model="state">
             <button class="w-8 h-8 align-middle" wire:click="$refresh" type="submit">
-
+                @if ($state == 1)
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z" />
                 </svg>
-            </button>
-        </form>
-        <form wire:submit.prevent="switchOff">
-            <input type="hidden" wire:model="state">
-            <button class="w-8 h-8 align-middle" wire:click="$refresh" type="submit">
-
+                @else
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                        d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
                 @endif
             </button>
         </form>
+
         @include('livewire.shared.editform')
+
         @if($state == 1)
         <form wire:submit.prevent="forceReset">
             <input type="hidden" wire:model="state">
